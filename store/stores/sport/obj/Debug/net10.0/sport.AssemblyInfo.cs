992718f86b10b30055ab174f793f3873c6f465c0
Bongani Dugmore Qunta <bonganidugmorequnta@gmail.com>
2026-01-20 18:51:10 +0000

    new residues

--- store/stores/sport/obj/Debug/net10.0/sport.AssemblyInfo.cs
+++ store/stores/sport/obj/Debug/net10.0/sport.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sport")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2dd1bc57936ec2f04b9d29e95f267ec893be911")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f71eb265f284ad5d9d7648d348140b5e49113314")]
 [assembly: System.Reflection.AssemblyProductAttribute("sport")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sport")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
